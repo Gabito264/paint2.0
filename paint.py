477d484610670b25022c7026021fed068c615a00
Gabriel Roque <56568228+Gabito264@users.noreply.github.com>
2022-05-10 12:14:59 -0500

    completado sección de triángulo

--- paint.py
+++ paint.py
@@ -48,6 +48,17 @@ def rectangle(start, end):
 
 def triangle(start, end):
     """Draw triangle from start to end."""
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(4):
+        if count != 2:
+            forward(end.x - start.x)
+            left(120)
+
+    end_fill()
     pass  # TODO
 
 
